27e32e2d0be2538ef2d04538ae316f90e7a95aae
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-07-23 01:45:27 +0300

    Initial commit: setup Spring project

---
+++ src/main/java/com/system/seogenix/SeOgenixApplication.java
@@ -0,0 +1,13 @@
+package com.system.seogenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeOgenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeOgenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/seogenix/SeOgenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.seogenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeOgenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
